3859c88c97482d71d5ace744597377c1f5f9b71f
CMU02 <hyeonjun1968@naver.com>
2026-05-29 14:18:19 +0900

    fix: resolve pr conflicts

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { AppProviders } from "./providers";
+
 import { NaverAccountList } from "@/features/naver-accounts/naver-account-list";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <NaverAccountList />
+    <AppProviders>
+      <NaverAccountList />
+    </AppProviders>
   </React.StrictMode>,
 );
